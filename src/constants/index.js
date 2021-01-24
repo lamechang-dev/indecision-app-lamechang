ed36aeccfa8dad7245f790610f1a63fb38135be1
lamechang-van <yoshimoto2107@gmail.com>
2021-01-24 17:27:59 +0900

    [update]change the dismiss duration

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,10 @@
 export const OPTION_LIMIT = 5;
 export const DEFAULT_NOTIFICATION_COMPONENT = {
-    type: "default",
+    type: "info",
     container: "top-right",
     animationIn: ["animate__animated", "animate__fadeIn"],
     animationOut: ["animate__animated", "animate__fadeOut"],
     dismiss: {
-        duration: 5000,
+        duration: 1500,
     }
 }
