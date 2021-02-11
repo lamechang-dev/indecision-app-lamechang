aa6feb617e2c5033e35c34375cbe9ca25c639192
lamechang-van <yoshimoto2107@gmail.com>
2021-02-11 14:02:56 +0900

    [update]test spread assignment

---
+++ src/playground/spread_assignment.js
@@ -0,0 +1,14 @@
+// const person = {
+//   age: 21,
+//   name: "John"
+// };
+
+// const { age, name } = person;
+
+// console.log(name);
+
+const colors = ["green", "yellow"];
+
+const [color1, color2] = colors;
+
+console.log(color1);
